3c39d06db1fe35dcdd10c5c54ca474df5c1e5784
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-04-30 17:45:04 +0200

    feat - set good params - learning rate, iterations count, number of hidden nodes, number of output nodes

--- my_answers.py
+++ my_answers.py
@@ -176,7 +176,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 5000
+learning_rate = 1
+hidden_nodes = 20
 output_nodes = 1
